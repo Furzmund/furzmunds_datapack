e8afa92216e7ff5e4c52af6f4b3161cd92993ad7
Furzmund <20493918+Furzmund@users.noreply.github.com>
2025-07-19 15:51:41 -0600

    Feature - Update to MC 1.21.0 - 1.21.1 (#7)

---
+++ make_datapack.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pack_name='fd'
+pack_version='2.3.0'
+mc_versions='1.20.5-1.21.1'
+target_files="data pack.* LICENSE README.md"
+zip -r "${pack_name}-${pack_version}_mc${mc_versions}.zip" ${target_files}
